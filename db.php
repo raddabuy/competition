09e3aabd939ebbdc20b397b34a666a5c62f5fa9c
raddabuy <raddabuy@yandex.ru>
2019-01-19 13:09:59 +0300

    Результаты распределяются по таблицам по полу участников

---
+++ db.php
@@ -0,0 +1,38 @@
+<?php
+    $user = 'root';
+    $password = '';
+    $db = 'testing';
+    $host = 'localhost';
+
+    $dsn = 'mysql:host='.$host.';dbname='.$db.';charset=utf8';
+    $pdo = new PDO($dsn, $user, $password);
+
+
+    $name = trim(filter_var($_POST['name'], FILTER_SANITIZE_STRING));
+    $score = trim(filter_var($_POST['score'], FILTER_SANITIZE_STRING));
+    $sex = trim(filter_var($_POST['sex'], FILTER_SANITIZE_STRING));
+
+
+    $sql = 'INSERT INTO competition(name, score, sex) VALUES(?, ?, ?)';
+    $query = $pdo->prepare($sql);
+    $query->execute([$name, $score, $sex]);
+
+    if($sex == 'male')
+    {
+      $sql = 'INSERT INTO male(name, score) VALUES(?, ?)';
+      $query = $pdo->prepare($sql);
+      $query->execute([$name, $score]);
+    }
+    else if($sex == 'female')
+    {
+      $sql = 'INSERT INTO female(name, score) VALUES(?, ?)';
+      $query = $pdo->prepare($sql);
+      $query->execute([$name, $score]);
+    }
+
+    header('Location:index.php');
+
+
+
+
+ ?>
